e0cfaa929c543bee1dea2df86d0f5eb22470aa05
Julia-Carbone <juliafernandacarbone@hotmail.com>
2023-08-23 11:08:40 -0300

    aula 24/25

--- src/main/java/com/julia/core/BaseTest.java
+++ src/main/java/com/julia/core/BaseTest.java
@@ -1,12 +1,39 @@
 package com.julia.core;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 public class BaseTest {
 
-    @After
-    public void tearDown() {
+    @Rule
+    public TestName testName = new TestName();
+
+
+    @AfterClass //executa apenas uma vez ao final de todos os metodos da classe
+    public static void finalizarClasse(){
         DriverFactory.killDriver();
     }
+    @After
+    public void tearDown() throws MalformedURLException {
+        gerarScreenShot();
+        DriverFactory.getDriver().resetApp(); //deixa os testes mais rapidos
+    }
 
+    public void gerarScreenShot() {
+        try {
+            File imagem = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(imagem, new File("target/screenshots/"+testName.getMethodName()+".png"));
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
 }
